d4dc6c89a78ea793806deea8d67a48b17a1edc35
DivyanshTayal <99646061+DivyanshTayal@users.noreply.github.com>
2023-02-19 22:13:50 +0530

    Add files via upload

---
+++ Web D Project/index.js
@@ -0,0 +1,31 @@
+document.querySelector(".D").addEventListener("click",function(){
+  var audio=new Audio("crash.mp3");
+  audio.play();
+
+});
+
+document.querySelector(".I").addEventListener("click",function(){
+  var audio=new Audio("1.wav");
+  audio.play();
+});
+document.querySelector(".T").addEventListener("click",function(){
+  var audio=new Audio("2.mp3");
+  audio.play();
+});
+
+document.querySelector(".A").addEventListener("click",function(){
+  var audio=new Audio("tom-1.mp3");
+  audio.play();
+});
+document.querySelector(".Y").addEventListener("click",function(){
+  var audio=new Audio("3.mp3");
+  audio.play();
+});
+document.querySelector(".A").addEventListener("click",function(){
+  var audio=new Audio("tom-3.mp3");
+  audio.play();
+});
+document.querySelector(".L").addEventListener("click",function(){
+  var audio=new Audio("5.mp3");
+  audio.play();
+});
